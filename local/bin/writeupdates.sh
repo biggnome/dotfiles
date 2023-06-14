cd6c720f3eaae4f5048e622ca0f02377bd7452c7
biggnome <sandymsmall@gmail.com>
2023-06-13 21:55:15 -0700

    modified:   local/bin/writeupdates.sh

--- local/bin/writeupdates.sh
+++ local/bin/writeupdates.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# apt update
+apt update
 echo $(aptitude search '~U' | wc -l) > /home/sandy/.cache/updates
 
 # echo $(apt list --upgradable | expr $(wc -l) - 1) > /home/sandy/.cache/updates
