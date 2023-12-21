433a18d815981b9a3b072a28b7db6f5ba32df7f8
Sandy Small <sandy@debian-vm>
2023-12-20 21:44:18 -0800

    modified:   bspwm/bspwmrc 	modified:   wezterm.lua

--- wezterm.lua
+++ wezterm.lua
@@ -17,7 +17,7 @@ config.window_frame = {
   -- Whatever font is selected here, it will have the
   -- main font setting appended to it to pick up any
   -- fallback fonts you may have used there.
-  font = wezterm.font { family = 'Ubuntu NF', weight="Medium" },
+  font = wezterm.font { family = 'Ubuntu Mono', weight="Bold" },
 
   -- The size of the font in the tab bar.
   -- Default to 10.0 on Windows but 12.0 on other systems
@@ -115,6 +115,8 @@ config.font = wezterm.font 'JetBrainsMono Nerd Font'
 config.font_size = 10.5
 config.use_fancy_tab_bar = true
 config.hide_tab_bar_if_only_one_tab = true
+config.initial_cols = 120
+config.initial_rows = 36
 
 -- and finally, return the configuration to wezterm
 return config
