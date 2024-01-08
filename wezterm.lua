0864f54724803b74f6798784be02731af898fc05
biggnome <sandymsmall@gmail.com>
2024-01-07 20:30:23 -0800

    modified:   wezterm.lua

--- wezterm.lua
+++ wezterm.lua
@@ -57,11 +57,11 @@ config.window_frame = {
   -- Whatever font is selected here, it will have the
   -- main font setting appended to it to pick up any
   -- fallback fonts you may have used there.
-  font = wezterm.font { family = 'Monaspace Argon', weight="Bold" },
+  font = wezterm.font { family = 'Monaspace Neon', weight="Bold" },
 
   -- The size of the font in the tab bar.
   -- Default to 10.0 on Windows but 12.0 on other systems
-  font_size = 11,
+  -- font_size = 11,
 
   -- The overall background color of the tab bar when
   -- the window is focused
